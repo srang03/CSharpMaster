306cde3c88b393504af9bd111fc5fb8748e52540
srang03 <srang9503@gmail.com>
2023-05-12 17:45:04 +0900

    230512 stud

---
+++ 07_const_readonly/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _07_const_readonly
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReadOnly.STATIC_CODE); 
+
+            ReadOnly readOnly = new ReadOnly();
+            Console.WriteLine(readOnly.NORMAL_CODE);
+        }
+
+    }
+
+    public class ReadOnly
+    {
+        public readonly static double STATIC_CODE;
+        public readonly double NORMAL_CODE;
+        public static double CODE;
+
+
+        public ReadOnly()
+        {
+            // STATIC_CODE = 20;
+            NORMAL_CODE = 10;
+            CODE = 10;
+        }
+
+        static ReadOnly()
+        {
+            STATIC_CODE = 20;
+            // NORMAL_CODE = 10;
+            CODE = 20;
+        }
+    }
+}
---
+++ 08_overloading/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _08_overloading
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Overloading overloading_1 = new Overloading("Hi", 10);
+            Overloading overloading_2 = new Overloading("Bye", 2);
+
+            var res = overloading_1 * overloading_2;
+        }
+    }
+    class Overloading
+    {
+        string name;
+        int number;
+        public Overloading(string strName, int iNumber)
+        {
+            name = strName;
+            number = iNumber;
+        }
+
+        public static int operator *(Overloading o1, Overloading o2)
+        {
+            return o1.number * o2.number;
+        }
+    }
+}
