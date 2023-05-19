a588af48a532cd9629bc8b74fa04d514d2bac8af
srang03 <srang9503@gmail.com>
2023-05-19 17:55:06 +0900

    230519

--- 15_boxingAndUnboxing/Program.cs
+++ 15_boxingAndUnboxing/Program.cs
@@ -7,9 +7,9 @@ namespace _15_boxingAndUnboxing
         static void Main(string[] args)
         {
             ArrayList list = new ArrayList();
-            list.Add(1);
-            list.Add(3.141592);
-            list.Add("Hi");
+            list.Add(1); // 값타입인 정수가 참조 객체 (object)으로 박싱이 된 후 전달된다.
+            list.Add(3.141592); // 값타입인 실수가 참조 객체 (object)으로 박싱이 된 후 전달된다.
+            list.Add("Hi"); // 참조 타입은 참조
         }
     }
 }
